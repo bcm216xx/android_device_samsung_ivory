df228993e7b9fa99a395d7fd58695fcfa584826b
karthick111 <lensflare21@gmail.com>
2016-11-29 10:01:57 +0100

    ivoryss: Switch to common_mini_phone.mk
    minimal packages :)

--- cm.mk
+++ cm.mk
@@ -2,7 +2,7 @@
 PRODUCT_RELEASE_NAME := GalaxyMusic
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/ivoryss/device.mk)
